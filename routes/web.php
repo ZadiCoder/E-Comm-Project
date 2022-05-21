0b2c63d05304769826ac03afba36c6baec395aaa
Zadiraaj <mughalzahid462@gmail.com>
2022-05-21 08:39:37 +0500

    customise login page and make Admin Controller

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BaseController;
+use App\Http\Controllers\AdminController;
 
 
-Route::get('/home',[BaseController::class,'show'])->name('home');
+Route::get('/',[BaseController::class,'show'])->name('home');
 Route::get('/specialoffer',[BaseController::class,'specialOffer'])->name('specialOffer');
 Route::get('/delivery',[BaseController::class,'delivery'])->name('delivery');
 Route::get('/contact-us',[BaseController::class,'contact'])->name('contact');
 Route::get('/cart',[BaseController::class,'cart'])->name('cart');
 Route::get('/productView',[BaseController::class,'productView'])->name('productView');
-
+Route::get('/admin/login',[AdminController::class,'login'])->name('admin.login');
 
